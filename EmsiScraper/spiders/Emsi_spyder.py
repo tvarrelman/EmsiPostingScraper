ac80e4c31d989e3a38c31cdd83583f9899e7b552
Tanner Varrelman <varr3316@vandals.uidaho.edu>
2021-02-18 15:13:55 -0800

    added comments

--- EmsiScraper/spiders/Emsi_spyder.py
+++ EmsiScraper/spiders/Emsi_spyder.py
@@ -1,7 +1,7 @@
 """ 
 	Description: This spider scrapes Emsi's job board 
 	and saves the job postings to a JSON file. Saved fields
-	include a job title, a location, a team, a category,
+	include a job title, location, team, category, description, 
 	and a link to the job posting.
 	Author: Tanner Varrelman
 """
@@ -21,7 +21,7 @@ class EmsiSpider(scrapy.Spider):
 		urls = ["https://jobs.lever.co/economicmodeling/"]
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
-
+	# this function finds the job postings and accesses the associated link
 	def parse(self, response):
 		soup = BeautifulSoup(response.text, features="lxml")
 		# find the full list of postings
@@ -31,7 +31,7 @@ class EmsiSpider(scrapy.Spider):
 			link = posting.find('a', href=True)
 			yield scrapy.Request(link['href'], callback = self.parse_post)
 
-	# parse parses the document accessed from the request
+	# this function parses the specific job posting
 	def parse_post(self, response):
 		# parse the page
 		post_soup = BeautifulSoup(response.text, features="lxml") 
@@ -41,10 +41,14 @@ class EmsiSpider(scrapy.Spider):
 		title = post_headline.find('h2').text
 		# extract the position location
 		location = post_headline.find('div', attrs={'sort-by-time'}).text
+		if '/' in location:
+			location = location.strip('/')
 		# extract the position commitment
 		commitment = post_headline.find('div', attrs={'sort-by-commitment'}).text
 		# extract the team and category
 		team_cat = post_headline.find('div', attrs={'sort-by-team'}).text
+		if '/' in team_cat:
+			team_cat = team_cat.strip('/')
 		# team and category need to separated
 		if ' – ' in team_cat:
 			team, category = team_cat.split(' – ', 1)
@@ -65,17 +69,33 @@ class EmsiSpider(scrapy.Spider):
 			description = ' '.join(desc_list)
 		else:
 			description = desc_list[0]
+		# define the url and pull relevant info from this
+		link = response.url
+		if 'jobs.lever.co/' in link:
+			company_id = link.split('jobs.lever.co/')[1]
+			company = company_id.split('/')[0]
+			pos_id =  company_id.split('/')[1]
+		else:
+			company = None
+			pos_id = None
+		# declare todays date for the file name and col value
 		todays_date = datetime.today().strftime('%Y-%m-%d')
 		file_date = datetime.today().strftime('%Y_%m_%d')
+		# name of final file
 		file_name = 'EmsiJobPosts_' + file_date +'.json'
+		# data that we have retrieved
 		data = {
 				'Position Title': title,
 				'Position Location': location,
 				'Team': team,
 				'Category': category,
 				'Position Description': description,
-				'Retrieval Date': todays_date
+				'Retrieval Date': todays_date,
+				'Link': link,
+				'Company': company,
+				'id': pos_id
 				}
+		# write the data to a json file
 		with open(file_name, "a") as json_file:
 			json_file.write(json.dumps(data))
 			json_file.write('\n')
